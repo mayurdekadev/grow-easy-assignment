14cfe63b668c9631a50e1e6715306614db0f9ef7
mayurdekadev <mayurdeka.work@gmail.com>
2026-05-23 17:17:41 +0530

    Perfomance overview cards

--- src/pages/ManageLeads/manageLeads.component.jsx
+++ src/pages/ManageLeads/manageLeads.component.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import { Phone, Users, Target } from 'lucide-react';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 import styles from "./manageLeads.module.css"
 
 const ManageLeads = () => {
@@ -11,7 +14,68 @@ const ManageLeads = () => {
             </div>
             <span className={styles.contentHeader}>Perfomace Overview</span>
             <div>
-
+                <Grid container spacing={3} sx={{ width: "100%", px: 3, boxSizing: "border-box" }}>
+                    <Grid size={{ xs: 12, sm: 6, md: 3 }} sx={{}} sx={{ display: "flex" }}>
+                        <Paper sx={{ width: "100%", height: "100%", padding: "15px", borderRadius: "10px", background: "linear-gradient(to top left, #e9f3f4 0%, #ffffff 50%)" }}>
+                            <div className={styles.paperContent}>
+                                <div className={styles.paperTitle}>
+                                    <span className={styles.title}>
+                                        Total Leads
+                                    </span>
+                                    <Users size={17} color="Black" strokeWidth={0.8} />
+                                </div>
+                                <span className={styles.value}>
+                                    45
+                                </span>
+                            </div>
+                        </Paper>
+                    </Grid>
+                    <Grid size={{ xs: 12, sm: 6, md: 3 }} sx={{ display: "flex" }}>
+                        <Paper sx={{ width: "100%", height: "100%", padding: "15px", borderRadius: "10px", background: "linear-gradient(to top left, #e9f3f4 0%, #ffffff 50%)" }}>
+                            <div className={styles.paperContent}>
+                                <div className={styles.paperTitle}>
+                                    <span className={styles.title}>
+                                        Contacted Leads
+                                    </span>
+                                    <Phone size={17} color="Black" strokeWidth={0.8} />
+                                </div>
+                                <span className={styles.value}>
+                                    31
+                                </span>
+                            </div>
+                        </Paper>
+                    </Grid>
+                    <Grid size={{ xs: 12, sm: 6, md: 3 }} sx={{ display: "flex" }}>
+                        <Paper sx={{ width: "100%", height: "100%", padding: "15px", borderRadius: "10px", background: "linear-gradient(to top left, #e9f3f4 0%, #ffffff 50%)" }}>
+                            <div className={styles.paperContent}>
+                                <div className={styles.paperTitle}>
+                                    <span className={styles.title}>
+                                        Sales done
+                                    </span>
+                                    <Target size={17} color="Black" strokeWidth={0.8} />
+                                </div>
+                                <span className={styles.value}>
+                                    27
+                                </span>
+                            </div>
+                        </Paper>
+                    </Grid>
+                    <Grid size={{ xs: 12, sm: 6, md: 3 }} sx={{ display: "flex" }}>
+                        <Paper sx={{ width: "100%", height: "100%", padding: "15px", borderRadius: "10px", background: "linear-gradient(to top left, #e9f3f4 0%, #ffffff 50%)" }}>
+                            <div className={styles.paperContent}>
+                                <div className={styles.paperTitle}>
+                                    <span className={styles.title}>
+                                        Conversion Rate
+                                    </span>
+                                    <Target size={17} color="Black" strokeWidth={0.8} />
+                                </div>
+                                <span className={styles.value}>
+                                    87.09%
+                                </span>
+                            </div>
+                        </Paper>
+                    </Grid>
+                </Grid>
             </div>
         </div>
     );
